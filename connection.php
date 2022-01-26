9c9048581382142c72813e50224e8ddb4dc42bb2
dccorimanya <datatheyvit123@gmail.com>
2022-01-26 00:07:06 -0500

    deleting old files

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-  require_once './database_params.php';
-
-  $connection = new mysqli($host, $user, $password, $database);
-
-  if ($connection->connect_errno){
-    throw new RuntimeException('Error al conectar a la base de datos' . $connection-> connect_error);
-  }
-?>
--- database_params.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $host = 'localhost';
-  $user = 'root';
-  $password = 'mysql123';
-  $database = 'tasks';
-?>
