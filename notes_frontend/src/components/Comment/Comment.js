a6aca135cbe6ccae82e0d89255175bae4e9ac9ee
dccorimanya <datatheyvit123@gmail.com>
2022-02-16 13:39:23 -0500

    [add] comment component

---
+++ notes_frontend/src/components/Comment/Comment.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import { fechaActual, sendData } from "../../utils/Utils";
+import { Comments } from "../Comments/Comments";
+import { Input } from "../Input/Input";
+import "./Comment.css"
+
+const Comment = ({ name, profile_img, task_id}) => {
+
+    const [comentario, setComentario] = useState('')
+    const [open, setOpen] = useState(false)
+    const [comentarios, setComentarios] = useState([])
+
+    const session = JSON.parse(sessionStorage.getItem('Sesion_de_usuario'))
+
+    const now = fechaActual()
+    
+    const atCommentUrl = "http://localhost:3000/api/v1/atcomment.php"
+    const getCommentUrl = "http://localhost:3000/api/v1/getcomment.php"
+
+    const atComment =async () => {
+        const data = {
+            "user_id": session.user_id,
+            "task_id": task_id,
+            "content": comentario,
+            "created_at": now
+        }
+        console.log(data)
+        const cm = await sendData(atCommentUrl, data)
+        console.log(cm)
+    }
+
+    useEffect(()=>{
+        fetch(`${getCommentUrl}?task_id=${task_id}`)
+        .then(response => response.json())
+        .then(data=>{
+            setComentarios(data.items)
+            console.log(data)
+        })
+    },[])
+
+    return(
+        <div>
+            <Input
+            setState={setComentario}
+            value={comentario}
+            type="text"
+            placeholder="Escibe comentario"
+
+            />
+            <button className="button is-success is-rounded mb-4" onClick={atComment}>Agregar comentario</button>
+            <p className="comentario" onClick={()=>{open? setOpen(false):setOpen(true)}}>Ver comentarios</p>
+            {!! open && 
+               comentarios.map((item)=>(
+                <Comments key={item.id} {...item}/>
+               ))
+            }
+
+        </div>
+    )
+}
+
+export{Comment}
