f2dbc949514f457492df61edb44b09cd342c5a28
dccorimanya <datatheyvit123@gmail.com>
2022-02-16 13:37:48 -0500

    [Add] endpoints from update, delete, insert->users, tasks, comments

---
+++ notes_backend/api/v1/updatetask.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$dir_controller = dirname(__FILE__, 3);
+
+require_once $dir_controller . '/controllers/TaskController.php';
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
+$task_update = new TaskController();
+
+$task_update->updateTask();
