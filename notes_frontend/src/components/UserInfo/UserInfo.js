03058a667c465965333151fb37fd37fe13d1d70b
dccorimanya <datatheyvit123@gmail.com>
2022-02-17 00:30:46 -0500

    [ADD] user info component

--- notes_frontend/src/components/UserInfo/UserInfo.js
+++ notes_frontend/src/components/UserInfo/UserInfo.js
@@ -1,21 +1,33 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import './UserInfo.css'
 
 
 const UserInfo = ({profile_img, name, user_id}) => {
+
+  const session = JSON.parse(sessionStorage.getItem('Sesion_de_usuario'))
+
+  let ruta = ''
+  if (session.user_id === user_id) {
+    ruta = '/userprofile/'
+  }else{
+    ruta = '/profile/'
+  }
+
     return(
-        <div className="card-content">
-        <div className="media">
+
+        <div className="media is-flex is-align-items-center boxComment" >
           <div className="media-left">
-            <figure className="image is-48x48">
-              <img src={profile_img} alt="Foto de perfil"/>
-            </figure>
+            <NavLink to={`${ruta}${user_id}/${name}`}>
+              <img className="avatar" src={profile_img} alt="Foto de perfil"/>
+            </NavLink>  
+           
           </div>
           <div className="media-content">
-            <NavLink className="title is-4" to={`/profile/${user_id}/${name}`}>{name}</NavLink>
+            <NavLink className="title is-4" to={`${ruta}${user_id}/${name}`}>{name}</NavLink>
           </div>
         </div>
-        </div>
+
     )
 }
 
